e3b24fbdeb3fbda4b2fb87311a62df4b0abc05c1
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-06-04 16:06:00 +0200

    Flask connection established in main.py with empty functions

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-import os
-from ui import *
+from flask import Flask, render_template, redirect, request, url_for
+from logic import *
+
+app = Flask(__name__)
 
 
-def main():
-    while True:
-        os.system("clear")
-        print_menu()
-        try:
-            select_menu()
-        except KeyError:
-            print("Error: No such option")
+@app.route("/")
+def display_main_page():
+    return render_template("index.html")
+
+
+@app.route("/mentors")
+def display_mentors_and_schools():
+    pass
+
+
+@app.route("/all_school")
+def display_mentors_and_all_schools():
+    pass
+
+
+@app.route("/mentors_by_country")
+def display_mentors_by_country():
+    pass
+
+
+@app.route("/contacts")
+def display_school_and_contact_person():
+    pass
+
+
+@app.route("/applicants")
+def display_applicants():
+    pass
+
+
+@app.route("/applicants_and_mentors")
+def display_applicants_and_mentors():
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
